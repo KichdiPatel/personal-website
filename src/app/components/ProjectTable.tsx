c3d820bd240ef8d0874881191dc1311e5a86f35c
KichdiPatel <ayanpatel09@gmail.com>
2025-08-23 20:21:33 +0100

    update color to 347433

--- src/app/components/ProjectTable.tsx
+++ src/app/components/ProjectTable.tsx
@@ -25,17 +25,17 @@ export default function ProjectTable({ projects }: ProjectTableProps) {
         <tbody>
           {projects.map((project, index) => (
             <tr key={index} className="border-b border-foreground group">
-              <td className="group-hover:text-blue-600 transition-colors">
+              <td className="group-hover:text-[#347433] transition-colors">
                 <Link href={project.url} className="block w-full h-full" target="_blank" rel="noopener noreferrer">
                   {project.title}
                 </Link>
               </td>
-              <td className="group-hover:text-blue-600 transition-colors">
+              <td className="group-hover:text-[#347433] transition-colors">
                 <Link href={project.url} className="block w-full h-full" target="_blank" rel="noopener noreferrer">
                   {project.type}
                 </Link>
               </td>
-              <td className="group-hover:text-blue-600 transition-colors">
+              <td className="group-hover:text-[#347433] transition-colors">
                 <Link href={project.url} className="block w-full h-full" target="_blank" rel="noopener noreferrer">
                   {project.description}
                 </Link>
